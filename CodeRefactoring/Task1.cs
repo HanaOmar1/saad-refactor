16ab897300b381ec3473faa66a3ecc9213aff30f
HanaOmar1 <Hanaomar20043@gmail.com>
2024-12-12 14:19:29 +0200

    task1

--- CodeRefactoring/Task1.cs
+++ CodeRefactoring/Task1.cs
@@ -15,25 +15,27 @@ namespace CodeRefactoring
     {
         public int CurrentAttempt { get; private set; } = 0;
         public bool IsLockedOut { get; private set; }
-
+        const int MAXNUM= 3;
         public bool Login(string password)
         {
-            if (!IsLockedOut)
+            if (IsLockedOut)
+                return false;
+
+            if (password == "password123")
             {
-                if (password == "password123")
-                {
-                    Console.WriteLine("Login successful!");
-                    return true;
-                }
-                else
-                {
-                    CurrentAttempt++;
+                Console.WriteLine("Login successful!");
+                return true;
+            }
+            else
+            {
+                CurrentAttempt++;
+                int remain_attempts = MAXNUM - CurrentAttempt;
 
                     // check the remaining attempts
-                    if (3 - CurrentAttempt > 0)
+                    if (remain_attempts > 0)
                     {
                         //print the remaining attempts
-                        Console.WriteLine("Invalid password. Remaining attempts: " + (3 - CurrentAttempt));
+                        Console.WriteLine("Invalid password. Remaining attempts: " + (remain_attempts));
                     }
                     else
                     {
@@ -41,9 +43,9 @@ namespace CodeRefactoring
                         Console.WriteLine("Invalid password. Account locked.");
                     }
                     return false;
-                }
             }
-            return false;
+            
+            
         }
     }
 }
